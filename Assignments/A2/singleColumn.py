c44729dc0e461a78b58af455b7fb142d9478baa3
Hrishi29 <31528997+Hrishi29@users.noreply.github.com>
2018-02-13 20:19:12 -0500

    small updates

--- Assignments/A2/singleColumn.py
+++ Assignments/A2/singleColumn.py
@@ -4,18 +4,16 @@ with open('counts.csv', 'a+', newline='') as csvfile:
 	spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 	spamwriter.writerow('Memento_Count')
 	csvfile.close()
-		
+#reading rows from csv file		
 with open('names.csv', newline='') as csvfile:
 	field = ['Memento_Count', 'URI_Count']
 	reader = csv.DictReader(csvfile, fieldnames=field)
-	for row in reader:
-		#count = row['Memento'] 
+	for row in reader: 
 		if row['URI_Count'] == "URI_Count":
-			continue
-	#	print(row['Memento_Count']) 	
+			continue 	
 		count = row['URI_Count']
 		count = int(count)
-		
+#storing all the mementos in single column based on their counts		
 		for row1 in range(0,count):
 			
 			with open('counts.csv', 'a+', newline='') as csvfile:
